9c5556e94da976c01115c4d3822880d105131204
QinT6o <wrytinter@yeah.net>
2025-05-12 23:02:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import tkinter as tk
-import mod_textCompar
-import mod_guide
-from mod_pasteTool import PasteAssistant
+from pasteTool import PasteAssistant
+from ui import Win as MainWin
+from control import Controller as MainUIController
 
-
-def main():
-
-    root = tk.Tk()
-    root.title("粘贴工具_v.2.0.qin")
-    root.wm_minsize(400, 500)
-    root.geometry("400x500")
-    app = PasteAssistant(root)
-    mod_textCompar.create_window(app.notebook)
-    mod_guide.create_guide_tab(app.notebook)
-    root.protocol("WM_DELETE_WINDOW", app.on_closing)
-    root.mainloop()
-
-
+app = MainWin(MainUIController())
 if __name__ == "__main__":
-    main()
+    paste_assistant = PasteAssistant()
+    paste_assistant.set_win(app)  # 确保传递的是当前显示的窗口
+    app.mainloop()
